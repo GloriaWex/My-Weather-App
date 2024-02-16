906590f57e5fe70d4955ea64f42f98f789efea53
GloriaWex <153610689+GloriaWex@users.noreply.github.com>
2024-02-16 21:48:13 +0100

    added weather forecast API integration

--- src/index.js
+++ src/index.js
@@ -57,24 +57,33 @@ function handleSearchSubmit(event) {
 
   searchCity(searchInput.value);
 }
+
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[date.getDay()];
+}
+
 function getForecast(city) {
   let apiKey = "ae01ceb742e8c6o415tcf3d5baf7b7b5";
   let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}&units=metric`;
   axios(apiUrl).then(displayForecast);
 }
+
 function displayForecast(response) {
-  console.log(response.data);
   let forecastElement = document.querySelector("#forecast");
 
-  let forecasttHtml = "";
-  response.data.daily.forEach(function (day) {
-    forecastHtml =
-      forecastHtml +
-      `
+  let forecastHtml = "";
+  response.data.daily.forEach(function (day, index) {
+    if (index < 5) {
+      forecastHtml =
+        forecastHtml +
+        `
   <div class="weather-forecast-day">
-    <div class="weather-forecast-date">Tue</div>
+    <div class="weather-forecast-date">${formatDay(day.time)}</div>
     <div>
-    <img src = "${day.condition.icon_url}"  class="weather-forecast-icon"/>
+    <img src = "${day.condition.icon_url}" class="weather-forecast-icon"/>
     </div>
     <div class="weather-forecast-temperatures">
       <div class="weather-forecast-temperature">
@@ -86,6 +95,7 @@ function displayForecast(response) {
     </div>
   </div>
 `;
+    }
   });
   forecastElement.innerHTML = forecastHtml;
 }
